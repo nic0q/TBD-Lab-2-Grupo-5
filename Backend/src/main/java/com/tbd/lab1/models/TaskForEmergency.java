609e3d38c8c3444cbbe18444b2a4d136e86e9554
Vanina Correa <vanina.correa@usach.cl>
2022-11-12 20:20:56 -0300

    [fix]: Minimal errors in query for repository TaskForEmergency

--- Backend/src/main/java/com/tbd/lab1/models/TaskForEmergency.java
+++ Backend/src/main/java/com/tbd/lab1/models/TaskForEmergency.java
@@ -1,16 +1,11 @@
 package com.tbd.lab1.models;
 
 public class TaskForEmergency {
-    private Integer id_task_for_emergency;
     private Integer id_emergency;
     private String emergency_details;
     private Integer id_task;
-    private String name_task;
+    private String name;
     private Integer quanty;
-
-    public Integer getId_task_for_emergency() {
-        return id_task_for_emergency;
-    }
 
     public Integer getId_emergency() {
         return id_emergency;
@@ -25,15 +20,11 @@ public class TaskForEmergency {
     }
 
     public String getName_task() {
-        return name_task;
+        return name;
     }
 
     public Integer getQuanty() {
         return quanty;
-    }
-
-    public void setId_task_for_emergency(Integer id_task_for_emergency) {
-        this.id_task_for_emergency = id_task_for_emergency;
     }
 
     public void setId_emergency(Integer id_emergency) {
@@ -49,7 +40,7 @@ public class TaskForEmergency {
     }
 
     public void setName_task(String name_task) {
-        this.name_task = name_task;
+        this.name = name_task;
     }
 
     public void setQuanty(Integer quanty) {
@@ -59,11 +50,10 @@ public class TaskForEmergency {
     @Override
     public String toString() {
         return "TaskForEmergency{" +
-                "id_task_for_emergency=" + id_task_for_emergency +
                 ", id_emergency=" + id_emergency +
                 ", emergency_details='" + emergency_details + '\'' +
                 ", id_task=" + id_task +
-                ", name_task='" + name_task + '\'' +
+                ", name_task='" + name + '\'' +
                 ", quanty=" + quanty +
                 '}';
     }
