9ee35447c1d85e4f6a175e1a83b5c4f8c9c750a0
LeVixo <91557877+LeVixo@users.noreply.github.com>
2022-11-13 17:53:23 -0300

    [feat]: Vista topEmergencies

--- Frontend/pages/topEmergencies.vue
+++ Frontend/pages/topEmergencies.vue
@@ -8,114 +8,50 @@
         <Navbar></Navbar>
         <div class="home">
             <h1 style="margin: 70px">Emergencias con menos voluntarios</h1>
-            <div class="row">
-                <table class="table table-dark" style="width: 1000px">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Emergencia</th>
-                            <th scope="col">Tareas</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Terremoto</td>
-                            <td>
-                                <table class="table table-dark" style="width: 1000px">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#ID</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">N° Voluntarios</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Limpiar escombros</td>
-                                            <td>4</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Rescatar afectados</td>
-                                            <td>2</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Buscar personas</td>
-                                            <td>3</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Tsunami</td>
-                            <td>
-                                <table class="table table-dark" style="width: 1000px">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#ID</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">N° Voluntarios</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Asistir gente</td>
-                                            <td>8</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Hablar con afectados</td>
-                                            <td>7</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Buscar personas</td>
-                                            <td>5</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Huracan</td>
-                            <td>
-                                <table class="table table-dark" style="width: 1000px">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">#ID</th>
-                                            <th scope="col">Nombre</th>
-                                            <th scope="col">N° Voluntarios</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Limpiar escombros</td>
-                                            <td>12</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Rescatar afectados</td>
-                                            <td>20</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Buscar personas</td>
-                                            <td>19</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div class="row" style="height: 700px">
+                <div class="col-md-6">
+                    <table class="table" style="width: 800px">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">ID</th>
+                                <th scope="col">Emergencia(s)</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Latitud / Longitud</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(emergency, index) in this.emergencies" :value="emergency.id_emergency"
+                                :key="index" class="table-light">
+                                <img class="check_button" v-on:click="get_tasks(emergency.id_emergency)"
+                                    style="margin-left: 10px; margin-top: 10px;" src="../static/check30.png" alt="Eye" />
+                                <td>{{ emergency.id_emergency }}</td>
+                                <td>{{ emergency.emergency_details }}</td>
+                                <td>{{ emergency.status }}</td>
+                                <td>({{ emergency.latitud }}, {{ emergency.longitud }})</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <table table class="table" style="width: 800px">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Tarea(s)</th>
+                                <th scope="col">N° Voluntarios</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(task, index) in this.tasks" :value="task.id_task"
+                                :key="index" class="table-light">
+                                <td>{{ task.id_task }}</td>
+                                <td>{{ task.name_task }}</td>
+                                <td>{{ task.quantity }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -132,7 +68,6 @@ var LeafIcon = L.Icon.extend({
 var myIcon = new LeafIcon({ iconUrl: icon });
 //librería axios
 
-import axios from 'axios';
 import Navbar from "../components/Navbar.vue";
 export default {
     components: {
@@ -143,7 +78,8 @@ export default {
         return {
             latitud: null, //Datos de nuevo punto
             longitud: null,
-            regiones: [], //Datos de regiones
+            emergencies: [], //Datos de regiones
+            tasks: [],
             name: '',
             points: [], //colección de puntos cargados de la BD
             message: '',
@@ -151,6 +87,30 @@ export default {
             id_region: 0
         }
     },
+    methods: {
+        async get_emergencies() {
+          try {
+            let response = await this.$axios.get("/emergencies");
+            this.emergencies = response.data;
+          } catch (error) {
+            console.log("error", error);
+          }
+        },
+        async get_tasks(id_emergency){
+            try {
+            let response = await this.$axios.get("/tasks-for-emergency/"  + id_emergency)
+            this.tasks = response.data;
+            } catch (error) {
+            console.log("error", error);
+          }
+        }
+    },
+    mounted: function () {
+        this.get_emergencies();
+        
+    //Se agregan los puntos mediante llamada al servicio
+    //this.getPoints(this.mymap, this.id_voluntario);
+  }
 }
 </script>
   
